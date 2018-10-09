9644846e4697eaf590fa7f75c63685ee41fdb53d
Andy Guo <andyguo@phenom-films.com>
2018-10-09 14:01:37 +0800

    learn from flask.g, threading isolation

--- dayu_database/__init__.py
+++ dayu_database/__init__.py
@@ -15,22 +15,25 @@ from deco import lazy
 _database_context = {}
 
 
-def get_session(name=None):
-    return DayuDatabase(name).session
+def get_session(db=None):
+    return DayuDatabase(db).session
 
 
 class DayuDatabase(object):
-    def __new__(cls, name=None):
-        name = name or os.environ.get(DAYU_DB_NAME, None) or 'default'
-        if name in _database_context:
-            return _database_context[name]
-        instance = super(DayuDatabase, cls).__new__(cls, name=name)
-        _database_context[name] = instance
+    def __new__(cls, db=None):
+        import threading
+        db = db or os.environ.get(DAYU_DB_NAME, None) or 'default'
+        current_threading_id = id(threading.current_thread())
+        uid = '{}_{}'.format(db, current_threading_id)
+        if uid in _database_context:
+            return _database_context[uid]
+        instance = super(DayuDatabase, cls).__new__(cls, name=db)
+        _database_context[uid] = instance
         return instance
 
-    def __init__(self, name=None):
-        name = name or os.environ.get(DAYU_DB_NAME, None) or 'default'
-        db_url_file = os.sep.join((os.path.dirname(__file__), 'static', 'db_url', name + '.json'))
+    def __init__(self, db=None):
+        db = db or os.environ.get(DAYU_DB_NAME, None) or 'default'
+        db_url_file = os.sep.join((os.path.dirname(__file__), 'static', 'db_url', db + '.json'))
 
         if not os.path.exists(db_url_file):
             from error import DayuDatabaseConfigNotExistError
@@ -69,6 +72,10 @@ if __name__ == '__main__':
     session = get_session()
     from table import *
 
-    aa = session.query(FOLDER).first()
+    aa = session.query(FILE).get(1148725585083440309)
     # print aa.test()
-    print aa
+    print aa.disk_path()
+    print type(aa.sub_level)
+    # print aa.path_data
+    for x in aa.sub_level.walk():
+        print x

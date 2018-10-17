20849829380d12b53fea96b29960ff9a2ebbead5
Andy Guo <andyguo@phenom-films.com>
2018-10-17 13:39:42 +0800

    fix

--- dayu_database/config/__init__.py
+++ dayu_database/config/__init__.py
@@ -4,6 +4,8 @@ from dayu_database.status import DayuDatabaseStatusNotConnect
 
 __author__ = 'andyguo'
 
+from const import *
+
 # 所有可能的分支条件，用于批量创建json config 的成绩结构。用户也可以通过手动创建文件的方式进行添加。
 DECISION_TREE = [['maya', 'nuke', 'houdini', 'hiero'],
                  ['default'],
@@ -17,35 +19,35 @@ class DayuDatabaseConfig(dict):
         if self['parent'].status is DayuDatabaseStatusNotConnect:
             super(DayuDatabaseConfig, self).__setitem__(key, value)
         else:
-            from error import DayuDatabaseConfigChangedAfterConnect
+            from dayu_database.error import DayuDatabaseConfigChangedAfterConnect
             raise DayuDatabaseConfigChangedAfterConnect('cannot change config after database connected!')
 
     def update(self, **kwargs):
         if self['parent'].status is DayuDatabaseStatusNotConnect:
             super(DayuDatabaseConfig, self).update(**kwargs)
         else:
-            from error import DayuDatabaseConfigChangedAfterConnect
+            from dayu_database.error import DayuDatabaseConfigChangedAfterConnect
             raise DayuDatabaseConfigChangedAfterConnect('cannot change config after database connected!')
 
     def pop(self, k):
         if self['parent'].status is DayuDatabaseStatusNotConnect:
             return super(DayuDatabaseConfig, self).pop(k)
         else:
-            from error import DayuDatabaseConfigChangedAfterConnect
+            from dayu_database.error import DayuDatabaseConfigChangedAfterConnect
             raise DayuDatabaseConfigChangedAfterConnect('cannot change config after database connected!')
 
     def popitem(self):
         if self['parent'].status is DayuDatabaseStatusNotConnect:
             return super(DayuDatabaseConfig, self).popitem()
         else:
-            from error import DayuDatabaseConfigChangedAfterConnect
+            from dayu_database.error import DayuDatabaseConfigChangedAfterConnect
             raise DayuDatabaseConfigChangedAfterConnect('cannot change config after database connected!')
 
     def setdefault(self, k, default=None):
         if self['parent'].status is DayuDatabaseStatusNotConnect:
             return super(DayuDatabaseConfig, self).setdefault(k, default)
         else:
-            from error import DayuDatabaseConfigChangedAfterConnect
+            from dayu_database.error import DayuDatabaseConfigChangedAfterConnect
             raise DayuDatabaseConfigChangedAfterConnect('cannot change config after database connected!')
 
     def __init__(self, **kwargs):
@@ -56,7 +58,7 @@ class DayuDatabaseConfig(dict):
                              'default'
         self[DAYU_CONFIG_STATIC_PATH] = kwargs.get(DAYU_CONFIG_STATIC_PATH, None) or \
                                         os.environ.get(DAYU_CONFIG_STATIC_PATH, None) or \
-                                        os.sep.join([os.path.dirname(__file__), 'static'])
+                                        os.sep.join([os.path.dirname(os.path.dirname(__file__)), 'static'])
 
     def from_json(self, path):
         import json
